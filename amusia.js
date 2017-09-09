87280325261ec061f7238af3374bd02d92c6d407
sonofthort <sonofthort@gmail.com>
2017-09-09 12:33:23 -0500

    some refactoring and new features.

--- amusia.js
+++ amusia.js
@@ -2,14 +2,21 @@ var amusia = {}
 
 amusia.tau = 6.283185307179586476925286766559
 
-amusia.Wave = function(sampleRate, data) {
-	this.data = data || []
-	this.sampleRate = sampleRate ? sampleRate : 22050
-	this.duration = 0
+amusia.Wave = function(args) {
+	this.sampleRate = args.sampleRate ? args.sampleRate : 22050
+	this.data = args.data ? args.data : []
+	this.duration = args.duration ? args.duration : 0
+	this.voice = args.voice
+	this.volume = args.volume
+	this.pan = args.pan
 }
 
 amusia.Wave.prototype = {
 	addNote: function(frequency, seconds, voiceFunc, volumeFunc, panFunc) {
+		voiceFunc = voiceFunc || this.voice
+		volumeFunc = volumeFunc || this.volume
+		panFunc = panFunc || this.pan
+	
 		var sampleRate = this.sampleRate,
 			data = this.data,
 			duration = this.duration,
@@ -29,6 +36,17 @@ amusia.Wave.prototype = {
 			data.push(right)
 		}
 
+		this.duration += seconds
+	},
+	addRest: function(seconds) {
+		var data = this.data,
+			numPoints = this.sampleRate * seconds
+		
+		for (var i = 0; i < numPoints; ++i) {
+			data.push(0)
+			data.push(0)
+		}
+		
 		this.duration += seconds
 	},
 	toRiffWave: function() {
@@ -73,16 +91,19 @@ amusia.Wave.prototype = {
 }
 
 amusia.Wave.deserialize = function(data) {
-	var res = new amusia.Wave(data.sampleRate, data.data)
-	res.duration = data.duration
-	return res
+	return new amusia.Wave({
+		sampleRate: data.sampleRate,
+		data: data.data,
+		duration: data.duration
+	})
 }
 
 amusia.Wave.mix = function(waves, finalize) {
 	var length = waves.length,
 		datas = waves.map(function(wave) {return wave.data}),
 		minSize = datas[0].length,
-		sampleRate = waves[0].sampleRate
+		sampleRate = waves[0].sampleRate,
+		duration = waves[0].duration
 	
 	for (var i = 1; i < length; ++i) {
 		if (waves[i].sampleRate !== sampleRate) {
@@ -90,6 +111,7 @@ amusia.Wave.mix = function(waves, finalize) {
 		}
 		
 		minSize = Math.min(minSize, datas[i].length)
+		duration = Math.min(duration, waves[i].duration)
 	}
 	
 	var data,
@@ -121,7 +143,11 @@ amusia.Wave.mix = function(waves, finalize) {
 		}
 	}
 	
-	return new amusia.Wave(sampleRate, data)
+	return new amusia.Wave({
+		sampleRate: sampleRate,
+		data: data,
+		duration: duration
+	})
 }
 
 amusia.constant = function(value) {
@@ -130,6 +156,9 @@ amusia.constant = function(value) {
 	}
 }
 
+// returns a function that takes a numeric "note number", and converts it to its cooresponding frequency,
+// in the requested temperament.
+// ex: 0 = C, 1 = C#, 2 = D, etc
 amusia.equalTemperamentPlayer = function(notesPerOctave, offset) {
 	return function(note) {
 		return Math.pow(2, Math.floor(note + offset) / notesPerOctave)
@@ -147,23 +176,23 @@ amusia.voice.xForm = function(func) {
 	}
 }
 
+amusia.voice.mix = function(voices) {
+	var numVoices = voices.length
+	
+	return function(f, t) {
+		var total = 0
+		
+		for (var i = 0; i < numVoices; ++i) {
+			total += voices[i](f, t)
+		}
+	
+		return total / numVoices
+	}
+}
+
 amusia.voice.exponentiate = function(voice, exponent) {
 	return function(f, t) {
 		return Math.pow(voice(f, t), exponent)
-	}
-}
-
-amusia.voice.split = function(voiceA, voiceB) {
-	var sine = amusia.voices.sine
-	
-	return function(f, t) {
-		return (sine(f, t) > 0 ? voiceA : voiceB)(f, t)
-	}
-}
-
-amusia.voice.mix = function(a, b, interval) {
-	return function(f, t) {
-		return ((t % f) > (interval * 0.5) ? a : b)(f, t)
 	}
 }
 
@@ -172,6 +201,20 @@ amusia.voice.zapper = function(exponent) {
 	return amusia.voice.xForm(function(x) {
 		return Math.sin(x + Math.sin(Math.pow(x, exponent)))
 	})
+}
+
+amusia.voice.waveSplit = function(voiceA, voiceB) {
+	var sine = amusia.voices.sine
+	
+	return function(f, t) {
+		return (sine(f, t) > 0 ? voiceA : voiceB)(f, t)
+	}
+}
+
+amusia.voice.timeSplit = function(a, b, interval) {
+	return function(f, t) {
+		return ((t % f) > (interval * 0.5) ? a : b)(f, t)
+	}
 }
 
 amusia.voices = {}
@@ -215,6 +258,7 @@ amusia.voices.flatBass = amusia.voice.xForm(function(x) {
 
 amusia.voices.silent = amusia.constant(0)
 
+// find source of this
 amusia.voices.violin = function(f, t) {
 	var y = 0,
 		A_total = 0
@@ -235,12 +279,14 @@ amusia.voices.violin = function(f, t) {
 	return res
 }
 
+// https://pages.mtu.edu/~suits/clarinet.html
+// ask for permission above before using this in proprietary software
 amusia.voices.clarinet = function(f, t) {
 	var w = f * t * amusia.tau,
 		sin = Math.sin
 	
 	// Odd harmonics
-	var res = (sin(w) + 0.75*sin(w*3) + 0.5*sin(w*5)+0.14*sin(w*7)+0.5*sin(w*9)+0.12*sin(11*w)+0.17*sin(w*13))/(1+.75+.5+.14+.17)
+	var res = (sin(w) + 0.75 * sin(w * 3) + 0.5 * sin(w * 5) + 0.14 * sin(w * 7) + 0.5 * sin(w * 9) + 0.12 * sin(11 * w) + 0.17 * sin(w * 13)) / (1 + .75 + .5 + .14 + .17)
 	
 	res *= Math.exp(t / 1.5)
 	res *= Math.exp(-t * 1.5)
@@ -253,8 +299,8 @@ amusia.voices.zapper_1_2 = amusia.voice.zapper(1 / 2)
 amusia.voices.zapper_2_3 = amusia.voice.zapper(2 / 3)
 amusia.voices.zapper_3_2 = amusia.voice.zapper(3 / 2)
 
-amusia.voices.circular_split_zappy_2_3 = amusia.voice.split(amusia.voices.circular, amusia.voices.zapper_2_3)
-amusia.voices.circular_mix_zappy_2_3 = amusia.voice.mix(amusia.voices.circular, amusia.voices.zapper_2_3)
+amusia.voices.circular_split_zappy_2_3 = amusia.voice.waveSplit(amusia.voices.circular, amusia.voices.zapper_2_3)
+amusia.voices.circular_mix_zappy_2_3 = amusia.voice.timeSplit(amusia.voices.circular, amusia.voices.zapper_2_3)
 
 amusia.memoize = function(func) {
 	var cache = {}
@@ -271,3 +317,177 @@ amusia.memoize = function(func) {
 		return res
 	}
 }
+
+amusia.chord = function(args) {
+	var notes = args.notes,
+		numNotes = notes.length,
+		notesPerOctave = args.notesPerOctave
+		
+	return {
+		invertUp: function() {
+			var invertedNotes = new Array(numNotes)
+				
+			for (var i = 1; i < numNotes; ++i) {
+				invertedNotes[i - 1] = notes[i]
+			}
+			
+			invertedNotes[numNotes - 1] = notes[0] + notesPerOctave
+			
+			return amusia.chord({
+				notes: invertedNotes,
+				notesPerOctave: notesPerOctave
+			})
+		},
+		invertDown: function() {
+			var invertedNotes = new Array(numNotes)
+				
+			for (var i = 1; i < numNotes; ++i) {
+				invertedNotes[i] = notes[i - 1]
+			}
+			
+			invertedNotes[0] = notes[numNotes - 1] - notesPerOctave
+			
+			return amusia.chord({
+				notes: invertedNotes,
+				notesPerOctave: notesPerOctave
+			})
+		},
+		append: function(extraNotes) {
+			if (typeof extraNotes === 'number') {
+				extraNotes = [].slice.call(arguments)
+			}
+		
+			var appendedNotes = notes.slice()
+			
+			extraNotes.forEach(function(note) {
+				appendedNotes.push(note)
+			})
+			
+			return amusia.chord({
+				notes: appendedNotes,
+				notesPerOctave: notesPerOctave
+			})
+		},
+		transpose: function(n) {
+			return amusia.chord({
+				notes: notes.map(function(note) {
+					return note + n
+				}),
+				notesPerOctave: notesPerOctave
+			})
+		},
+		sort: function() {
+			return amusia.chord({
+				notes: notes.slice().sort(),
+				notesPerOctave: notesPerOctave
+			})
+		},
+		extend: function(numOctaves) {
+			var extendedNotes = []
+			
+			for (var i = 0; i <= numOctaves; ++i) {
+				var offset = i * notesPerOctave
+				
+				for (var j = 0; j < numNotes; ++j) {
+					extendedNotes.push(notes[j] + offset)
+				}
+			}
+			
+			extendedNotes.push(notes[0] + numOctaves * notesPerOctave)
+			
+			return amusia.chord({
+				notes: extendedNotes,
+				notesPerOctave: notesPerOctave
+			})
+		},
+		flipIntervals: function() {
+			return amusia.chord({
+				notes: notes.map(function(note) {
+					return notesPerOctave - note
+				}).sort(),
+				notesPerOctave: notesPerOctave
+			})
+		},
+		notes: function() {
+			return notes.slice()
+		},
+		transposeOctave: function(n) {
+			var offset = notesPerOctave * n
+			
+			return amusia.chord({
+				notes: notes.map(function(note) {
+					return note + offset
+				}),
+				notesPerOctave: notesPerOctave
+			})
+		}
+	}
+}
+
+amusia.chords = {
+	major: amusia.chord({
+		notes: [0, 4, 7],
+		notesPerOctave: 12
+	}),
+	minor: amusia.chord({
+		notes: [0, 3, 7],
+		notesPerOctave: 12
+	}),
+	diminished: amusia.chord({
+		notes: [0, 3, 6],
+		notesPerOctave: 12
+	}),
+	augmented: amusia.chord({
+		notes: [0, 4, 8],
+		notesPerOctave: 12
+	}),
+	flat5: amusia.chord({
+		notes: [0, 4, 6],
+		notesPerOctave: 12
+	}),
+	sus2: amusia.chord({
+		notes: [0, 2, 7],
+		notesPerOctave: 12
+	}),
+	sus4: amusia.chord({
+		notes: [0, 5, 7],
+		notesPerOctave: 12
+	})
+}
+
+amusia.chords.major7 = amusia.chords.major.append(10)
+amusia.chords.majorMajor7 = amusia.chords.major.append(11)
+amusia.chords.majorAdd2 = amusia.chords.major.append(2).sort()
+amusia.chords.majorAdd4 = amusia.chords.major.append(5).sort()
+amusia.chords.major7Add2 = amusia.chords.major7.append(2).sort()
+amusia.chords.majorMajor7Add2 = amusia.chords.majorMajor7.append(2).sort()
+amusia.chords.major7Add4 = amusia.chords.major7.append(5).sort()
+amusia.chords.majorMajor7Add4 = amusia.chords.majorMajor7.append(5).sort()
+amusia.chords.major6 = amusia.chords.major.append(9)
+amusia.chords.majorMinor6 = amusia.chords.major.append(8)
+amusia.chords.major9 = amusia.chords.major7.append(14)
+amusia.chords.majorMajor9 = amusia.chords.majorMajor7.append(14)
+amusia.chords.major11 = amusia.chords.major7.append(17)
+amusia.chords.majorMajor11 = amusia.chords.majorMajor7.append(17)
+
+amusia.chords.minor7 = amusia.chords.minor.append(10)
+amusia.chords.minorMajor7 = amusia.chords.minor.append(11)
+amusia.chords.minorAdd2 = amusia.chords.minor.append(2).sort()
+amusia.chords.minorAdd4 = amusia.chords.minor.append(5).sort()
+amusia.chords.minor7Add2 = amusia.chords.minor7.append(2).sort()
+amusia.chords.minorMajor7Add2 = amusia.chords.minorMajor7.append(2).sort()
+amusia.chords.minor7Add4 = amusia.chords.minor7.append(5).sort()
+amusia.chords.minorMajor7Add4 = amusia.chords.minorMajor7.append(5).sort()
+amusia.chords.minorMajor6 = amusia.chords.minor.append(9)
+amusia.chords.minor6 = amusia.chords.minor.append(8)
+amusia.chords.minor9 = amusia.chords.minor7.append(14)
+amusia.chords.minorMajor9 = amusia.chords.minorMajor7.append(14)
+amusia.chords.minor11 = amusia.chords.minor7.append(17)
+amusia.chords.minorMajor11 = amusia.chords.minorMajor7.append(17)
+
+amusia.chords.diminished7 = amusia.chords.diminished.append(9)
+amusia.chords.diminishedMinor7 = amusia.chords.diminished.append(10)
+amusia.chords.diminishedMajor7 = amusia.chords.diminished.append(11)
+
+amusia.chords.augmented7 = amusia.chords.augmented.append(10)
+amusia.chords.augmentedMajor7 = amusia.chords.augmented.append(11)
