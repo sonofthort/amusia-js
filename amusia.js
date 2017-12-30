1b2843206542dab0cf2de2f319c0c9965a517891
sonofthort <sonofthort@gmail.com>
2017-12-29 19:04:31 -0600

    added Wave.concat. some cleanup. added SoundSpritePlayer. added beginnings of amusia.song.

--- amusia.js
+++ amusia.js
@@ -2,6 +2,18 @@ var amusia = {}
 
 amusia.tau = 6.283185307179586476925286766559
 
+amusia.util = {}
+
+amusia.util.kv = function(obj, func) {
+	for (var k in obj) {
+		if (obj.hasOwnProperty(k)) {
+			func(k, obj[k], obj)
+		}
+	}
+	
+	return obj
+}
+
 amusia.Wave = function(args) {
 	this.sampleRate = args.sampleRate ? args.sampleRate : 22050
 	this.data = args.data ? args.data : []
@@ -99,13 +111,13 @@ amusia.Wave.deserialize = function(data) {
 }
 
 amusia.Wave.mix = function(waves, finalize) {
-	var length = waves.length,
+	var numWaves = waves.length,
 		datas = waves.map(function(wave) {return wave.data}),
 		minSize = datas[0].length,
 		sampleRate = waves[0].sampleRate,
 		duration = waves[0].duration
 	
-	for (var i = 1; i < length; ++i) {
+	for (var i = 1; i < numWaves; ++i) {
 		if (waves[i].sampleRate !== sampleRate) {
 			throw 'sampleRate mismatch'
 		}
@@ -115,7 +127,7 @@ amusia.Wave.mix = function(waves, finalize) {
 	}
 	
 	var data,
-		inverseLength = 1 / length
+		inverseNumWaves = 1 / numWaves
 	
 	if (finalize) {
 		data = new Uint8Array(minSize)
@@ -123,11 +135,11 @@ amusia.Wave.mix = function(waves, finalize) {
 		for (var i = 0; i < minSize; ++i) {
 			var res = 0
 			
-			for (var j = 0; j < length; ++j) {
+			for (var j = 0; j < numWaves; ++j) {
 				res += datas[j][i]
 			}
 			
-			data[i] = Math.round(127 * res * inverseLength) + 128
+			data[i] = Math.round(127 * res * inverseNumWaves) + 128
 		}
 	} else {
 		data = new Array(minSize)
@@ -135,11 +147,79 @@ amusia.Wave.mix = function(waves, finalize) {
 		for (var i = 0; i < minSize; ++i) {
 			var res = 0
 			
-			for (var j = 0; j < length; ++j) {
+			for (var j = 0; j < numWaves; ++j) {
 				res += datas[j][i]
 			}
 			
-			data[i] = res * inverseLength
+			data[i] = res * inverseNumWaves
+		}
+	}
+	
+	return new amusia.Wave({
+		sampleRate: sampleRate,
+		data: data,
+		duration: duration
+	})
+}
+
+amusia.Wave.concat = function(waves, bufferSeconds, finalize) {
+	bufferSeconds = bufferSeconds ? bufferSeconds : 0
+
+	var numWaves = waves.length,
+		datas = waves.map(function(wave) {return wave.data}),
+		sampleRate = waves[0].sampleRate,
+		buffer = Math.floor(bufferSeconds * sampleRate * 2),
+		duration = waves[0].duration + bufferSeconds,
+		size = datas[0].length + buffer
+	
+	for (var i = 1; i < numWaves; ++i) {
+		if (waves[i].sampleRate !== sampleRate) {
+			throw 'sampleRate mismatch'
+		}
+		
+		size += datas[i].length + buffer
+		duration += waves[i].duration + bufferSeconds
+	}
+	
+	var dataIndexBase = 0
+	
+	if (finalize) {
+		var data = new Uint8Array(size)
+	
+		for (var i = 0; i < numWaves; ++i) {
+			var waveData = datas[i],
+				waveDataLength = waveData.length
+			
+			for (var j = 0; j < waveDataLength; ++j) {
+				data[dataIndexBase + j] = Math.round(127 * waveData[j]) + 128
+			}
+			
+			dataIndexBase += waveDataLength
+			
+			for (var j = 0; j < buffer; ++j) {
+				data[dataIndexBase + j] = 128
+			}
+			
+			dataIndexBase += buffer
+		}
+	} else {
+		var data = new Array(size)
+	
+		for (var i = 0; i < numWaves; ++i) {
+			var waveData = datas[i],
+				waveDataLength = waveData.length
+			
+			for (var j = 0; j < waveDataLength; ++j) {
+				data[dataIndexBase + j] = waveData[j]
+			}
+			
+			dataIndexBase += waveDataLength
+			
+			for (var j = 0; j < buffer; ++j) {
+				data[dataIndexBase + j] = 0
+			}
+			
+			dataIndexBase += buffer
 		}
 	}
 	
@@ -319,11 +399,17 @@ amusia.memoize = function(func) {
 }
 
 amusia.chord = function(args) {
-	var notes = args.notes,
+	var notes = args.notes.slice(),
 		numNotes = notes.length,
 		notesPerOctave = args.notesPerOctave
 		
 	return {
+		serialize: function() {
+			return {
+				notes: notes.slice(),
+				notesPerOctave: notesPerOctave
+			}
+		},
 		invertUp: function() {
 			var invertedNotes = new Array(numNotes)
 				
@@ -491,3 +577,153 @@ amusia.chords.diminishedMajor7 = amusia.chords.diminished.append(11)
 
 amusia.chords.augmented7 = amusia.chords.augmented.append(10)
 amusia.chords.augmentedMajor7 = amusia.chords.augmented.append(11)
+
+amusia.song = {
+	create: function(args) {
+		var chords = {}
+		
+		amusia.util.kv(amusia.chords, function(k, v) {
+			chords[k] = v
+		})
+		
+		if (args.chords) {
+			var getChord = function(name) {
+				var chord = chords[name]
+				
+				if (chord) {
+					return chord
+				}
+				
+				var chordArgs = args.chords[name]
+				
+				if (chordArgs) {
+					if (chordArgs.base) {
+						var base = getChord(chordArgs.base)
+						
+						if (base == null) {
+							throw "unknown chord '" + chordArgs.base + "'"
+						}
+						
+						chord = base
+					} else {
+						chord = amusia.chord({
+							notes: chordArgs.notes,
+							notesPerOctave: chordArgs.notesPerOctave ? chordArgs.notesPerOctave : 12
+						})
+					}
+					
+					if (chordArgs.transform) {
+						chordArgs.transform.forEach(function(transformArgs) {
+							if (chord.hasOwnProperty(transformArgs[0])) {
+								chord = chord[transformArgs[0]](transformArgs[1])
+							} else {
+								throw "unknown transform '" + transformArgs[0] + "'"
+							}
+						})
+					}
+				}
+				
+				chords[name] = chord
+				
+				return chord
+			}
+		
+			amusia.util.kv(args.chords, getChord)
+		}
+		
+		var voices = {}
+		
+		amusia.util.kv(amusia.voices, function(k, v) {
+			voices[k] = v
+		})
+		
+		var chordProgressions = {}
+		
+		if (args.chordProgressions) {
+			amusia.util.kv(args.chordProgressions, function(k, v) {
+				chordProgressions[k] = v.map(function(chordName) {
+					var chord = chords[chordName]
+					
+					if (chord == null) {
+						throw "unknown chord '" + chordName + "'"
+					}
+					
+					return chord
+				})
+			})
+		}
+	},
+	deserialize: function(args) {
+		
+	}
+}
+
+amusia.SoundSpritePlayer = function(args) {
+	var poolSize = args.poolSize ? args.poolSize : 4,
+		volume = args.volume ? args.volume : 0.5,
+		src = args.src ? args.src : '',
+		sprites = args.sprites ? args.sprites : {},
+		time = args.time ? args.time : 0
+	
+	this.pool = dc.generate(poolSize, function() {
+		var audio = new Audio()
+		
+		audio.src = src
+		audio.volume = volume
+		
+		return audio
+	})
+	
+	this.activeAudios = []
+	this.endTimes = []
+	this.time = time
+	this.sprites = sprites
+}
+
+amusia.SoundSpritePlayer.prototype = {
+	play: function(name) {
+		if (this.pool.length === 0) {
+			var audio = this.activeAudios.shift()
+			audio.pause()
+			this.pool.push(audio)
+			this.endTimes.shift()
+		}
+		
+		var sprite = this.sprites[name]
+		
+		if (sprite == null) {
+			return false
+		}
+		
+		var audio = this.pool.pop()
+		audio.pause()
+		audio.currentTime = sprite.time
+		audio.play()
+		
+		this.activeAudios.push(audio)
+		this.endTimes.push(this.time + sprite.duration)
+		
+		return true
+	},
+	update: function(time) {
+		this.time = time
+		
+		var activeAudios = this.activeAudios,
+			endTimes = this.endTimes,
+			pool = this.pool
+		
+		for (var i = activeAudios.length - 1; i >= 0; --i) {
+			if (endTimes[i] <= time) {
+				endTimes.pop()
+				var audio = activeAudios.pop()
+				audio.pause()
+				pool.push(audio)
+			}
+		}
+	},
+	stop: function() {
+		var time = this.time
+		this.update(Number.POSITIVE_INFINITY)
+		this.time = time
+	}
+}
